25d93dd24a00de994cb00f78887e72b6e875f2b2
Andy Fundinger <andy@makerbar.com>
2012-12-08 18:03:09 -0500

    adjusted embed_base design to support single call to time.time() per loop. Prepared statistics function to show how fast the main loop is running at any given time.

--- pi/embed_base.py
+++ pi/embed_base.py
@@ -4,6 +4,7 @@ except ImportError:
     import RPiFake.GPIO as GPIO
 
 import time
+from collections import deque
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -18,18 +19,23 @@ blink_time = time.time()
 state_time = time.time()
 state = STARTUP
 
-def state_change(new_state):
+loop_time = time.time()
+loop_times = deque(maxlen=20)
+
+def state_change(new_state, time_at):
     global state, state_time, button_time
-    state_time = time.time()
+    state_time = time_at
     print "\n",state.name,"->",new_state.name,"\n"
     state = new_state
 
 while True:
-
+    last_loop = loop_time
+    loop_time = time.time()
     if state is READY:
         pass
     elif state is STARTUP:
-        state_change(READY  )
+        state_change(READY,loop_time)
 
-
-    print state.name,"\t\t",time.time()-state_time,"\r",
+    print state.name,"\t\t",loop_time-state_time,"\tLoop Time",loop_time-last_loop,"\r",
+    #loop_times.append(loop_time-last_loop)
+    #print "Max Loop",max(loop_times),"\tMin loop",min(loop_times),"\tAvg Loop",sum(loop_times)/len(loop_times),"\r",
